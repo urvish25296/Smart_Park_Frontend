333a69cd1994f5d408b23d82ae98705753b665c6
urvish25296 <urvish25296@gmail.com>
2021-04-12 00:32:53 -0400

    sw

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import reportWebVitals from "./reportWebVitals";
 import ApolloProvider from "./components/ApolloProvider";
 import * as serviceWorker from './serviceWorker';
 
@@ -14,5 +13,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 serviceWorker.register();
